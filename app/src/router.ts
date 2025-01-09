b0a35c03044dffe43d8e1ac0137e6ecaa69221ce
Jake Robins <me@jakerobins.com>
2025-01-09 09:01:29 -0600

    add kysely test

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,5 +1,21 @@
-import { Client } from "pg";
+import { Client, Pool } from "pg";
 import express from "express";
+import { Kysely, PostgresDialect, sql } from "kysely";
+import { DB } from "kysely-codegen";
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    database: "test",
+    host: "localhost",
+    user: "admin",
+    port: 5434,
+    max: 10,
+  }),
+});
+
+export const db = new Kysely<DB>({
+  dialect,
+});
 
 export const router = (client: Client) => {
   const router = express.Router();
@@ -7,6 +23,10 @@ export const router = (client: Client) => {
   router.get("/", async (req, res) => {
     const { rows } = await client.query("SELECT * FROM users");
     res.send(rows);
+
+    const authors = await db.selectFrom("authors").selectAll().execute();
+
+    const result = await sql`SELECT id name FROM authors`.execute(db);
   });
 
   return router;
